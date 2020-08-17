35623f99ebbbfa5dac7bd7c7278d06e7df4d8853
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-17 16:48:09 -0300

    Implementa View home.

---
+++ app/Views/home.php
@@ -0,0 +1,14 @@
+<html>
+
+    <head>
+        <title></title>
+    </head>
+    <style>
+        p {text-align: center;};
+    </style>
+    <body>
+
+    <p>Olá Mundo</p>
+    <center><img src="/imagens/cachorro-nao-bebe-agua-pet.jpg" width="500"></center>
+    </body>
+</html>
