d0682ade0d43a9b53717295af3028d78c78efac3
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-19 15:26:32 -0300

    Melhorou a Home e passou o CSS e JS para arquivos externos.

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,14 +1,36 @@
 <html>
 
     <head>
-        <title></title>
+        <title>Galeria da estagiária</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="/css/teste.css">
     </head>
-    <style>
-        p {text-align: center;};
-    </style>
+
     <body>
 
-    <p>Olá Mundo</p>
-    <center><img src="/imagens/cachorro-nao-bebe-agua-pet.jpg" width="500"></center>
+        <p>
+            Bem vindo a galeria de fotos da estagiária
+        </p>
+
+        <!-- Galeria -->
+        <div class="galeria">
+            <?php foreach ($imagens as $chave => $item) { ?>
+                <img src="<?php echo $item; ?>"  width="500" class="imagem" <?php echo ($chave > 0) ? 'style="display: none;"' : ''; ?> >
+            <?php } ?>
+        </div>
+
+        <!-- Controles -->
+        <div class="botões">
+            <button type="button" onclick="PassarAnterior();" class="botão">
+                Anterior
+            </button>
+            <button type="button" onclick="PassarProxima();" class="botão">
+                Próxima
+            </button>
+        </div>
+
+        <!-- JS -->
+        <script type="text/javascript" src="/js/teste.js"></script>
     </body>
 </html>
