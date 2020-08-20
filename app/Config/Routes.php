d49fee9a0a23599777cd7628efb69a53e8176fe0
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-20 17:02:52 -0300

    Define rotas.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,8 +30,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Galeria::index');
-$routes->add('galeria', 'Galerias::index');
+$routes->get('/', 'Galerias::index');
+$routes->add('galeria/(:num)', 'Galeria::index/$1');
 
 /**
  * --------------------------------------------------------------------
