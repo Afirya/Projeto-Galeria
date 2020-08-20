47211c31b4685b3a0873f821c8bbc4215133d760
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-20 16:14:36 -0300

    Implementa gulpfile com tarefas básicas.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+const {task, series} = require('gulp');
+
+task('css', function() {
+    return gulp.src([
+        './public/css/teste.css',
+        './public/css/teste-2.css',
+    ])
+    .pipe(concat('styles.css'))
+    .pipe( gulp.dest('public/dist/css'))
+});
+
+task('js', function() {
+    return gulp.src([
+        './public/js/teste.js',
+    ])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dist/js'))
+});
+
+task('default', series('css', 'js'));
+
+task('watch', function() {
+   gulp.watch(['./public/css/*.css','./public/js/*.js'], series('css','js'));
+});
