9ff20a0dc7c6179a27f4574ff7cc413585a0014d
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-20 16:12:04 -0300

    Renomeia e implementa template.

---
+++ app/Views/galeria.php
@@ -0,0 +1,27 @@
+<?php /** @var array $imagens */?>
+<?php $this->extend('template'); ?>
+
+<?php $this->section('content'); ?>
+    <p>
+        Bem vindo a galeria de fotos da estagiária
+    </p>
+
+    <!-- Galeria -->
+    <div class="galeria">
+        <?php foreach ($imagens as $chave => $item) { ?>
+            <img src="<?php echo $item; ?>"  width="500" class="imagem" <?php echo ($chave > 0) ? 'style="display: none;"' : ''; ?> >
+        <?php } ?>
+    </div>
+
+    <!-- Controles -->
+    <div class="botões">
+        <button type="button" onclick="PassarAnterior();" class="botão">
+            Anterior
+        </button>
+        <button type="button" onclick="PassarProxima();" class="botão">
+            Próxima
+        </button>
+    </div>
+
+<?php $this->endSection(); ?>
+
