46cfbdf6ca979678d918617b76f62036f6681b6a
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-21 16:45:06 -0300

    Implementa título genérico.

--- app/Views/template.php
+++ app/Views/template.php
@@ -1,7 +1,7 @@
 <?php /** @var string $titulo */?>
 <html>
     <head>
-            <title><?php echo $titulo; ?></title>
+            <title><?php echo $titulo ?? "G"; ?></title>
             <link rel="stylesheet" type="text/css" href="/dist/css/styles.css">
     </head>
     <body>
