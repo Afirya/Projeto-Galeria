c297da7bcedd78e3e3fe29c8f300167e6afd1b9e
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-21 16:06:00 -0300

    Refatora o controller galerias para usar o BD.

--- app/Controllers/Galerias.php
+++ app/Controllers/Galerias.php
@@ -2,15 +2,11 @@
 
 class Galerias extends BaseController
 {
-    public function index() {
-
-        $galerias = [
-            "Galeria 1",
-            "Galeria 2",
-            "Galeria 3",
-            "Galeria 4",
-            "Galeira 5"
-        ];
+    public function index()
+    {
+        $db = \Config\Database::connect();
+        $query = $db->query("select * from galerias;");
+        $galerias = $query->getResult();
 
         $data["galerias"] = $galerias;
         $data["titulo"] = "Lista de Galerias";
