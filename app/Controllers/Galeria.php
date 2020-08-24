5209bbfbcd0746437ea602bbba0042f5161e0102
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-24 15:02:23 -0300

    Refatora nome da chave.

--- app/Controllers/Galeria.php
+++ app/Controllers/Galeria.php
@@ -5,7 +5,7 @@ class Galeria extends BaseController
 	public function index($a)
 	{
         $db = \Config\Database::connect();
-        $query = $db->query("select * from fotos where idgaleria = $a;");
+        $query = $db->query("select * from fotos where id_galeria = $a;");
         $imagens = $query->getResult();
 
         $data["titulo"] = "Galeria";
