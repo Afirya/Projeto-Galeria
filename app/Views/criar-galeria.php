442d3baf2cdcb3418a9e6ed43affeba5e0564945
Paula Fraga <pfraga@laplace.epungo.net.br>
2020-08-25 13:12:30 -0300

    Implementa formulário para criar galeria.

---
+++ app/Views/criar-galeria.php
@@ -0,0 +1,17 @@
+<?php /** @var array $galerias */?>
+
+<?php $this->extend('template'); ?>
+
+<?php $this->section('content'); ?>
+    <h1>
+        Nova Galeria
+    </h1>
+
+    <form action="/galeria/criar" method="post">
+        <input type="text" name="nome" placeholder="Nome da Galeria">
+        <input type="submit" value="criar">
+    </form>
+
+<?php $this->endSection(); ?>
+
+
