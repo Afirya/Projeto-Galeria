5c526a94c083b13e79d937d096faceac7a9d2356
pfraga <pfraga@epungo.com.br>
2020-08-26 12:46:24 -0300

    Refatora código.

--- app/Controllers/Galeria.php
+++ app/Controllers/Galeria.php
@@ -18,6 +18,7 @@ class Galeria extends BaseController
     {
         $db = \Config\Database::connect();
         $query = $db->query("delete from galerias where id = $id;");
+
         return redirect()->to('/');
     }
 
@@ -35,6 +36,7 @@ class Galeria extends BaseController
             'nome' => $nome,
             'data' => $data
         ]);
+
         return redirect()->to('/');
     }
 
@@ -44,38 +46,43 @@ class Galeria extends BaseController
         $data["mensagem"] = "";
         echo view('subir-imagem',$data);
     }
+
     public function subir_imagem($id_galeria)
     {
-       $validação = $this->validate([
+        helper('text');
+
+        $validação = $this->validate([
             'imagem' => 'uploaded[imagem]|max_size[imagem,2000]|max_dims[imagem,718,482]|ext_in[imagem,jpg,jpeg,png]'
-        ]);
-       if($validação == false) {
-           $data["id_galeria"] = $id_galeria;
-           $data["mensagem"] = "Imagem fora do padrão, selecione outra.";
-           echo view('subir-imagem',$data);
-       } else {
-           helper('text');
+       ]);
 
-           $letras_aleatorias = random_string('alnum', 8);
+        if($validação == false)
+        {
+            $data["id_galeria"] = $id_galeria;
+            $data["mensagem"] = "Imagem fora do padrão, selecione outra.";
+            echo view('subir-imagem',$data);
+        }
+        else
+        {
+            $letras_aleatorias = random_string('alnum', 8);
 
-           $nome_da_imagem = implode('-', [
-               $id_galeria,
-               date('Y-m-d_H:i:s'),
-               $letras_aleatorias,
-               $_FILES["imagem"]["name"],
-           ]);
-           $caminho = FCPATH . 'imagens/' . $nome_da_imagem;
+            $nome_da_imagem = implode('-', [
+                $id_galeria,
+                date('Y-m-d_H:i:s'),
+                $letras_aleatorias,
+                $_FILES["imagem"]["name"],
+            ]);
+            $caminho = FCPATH . 'imagens/' . $nome_da_imagem;
 
+            $db = \Config\Database::connect();
+            $db->table('fotos')->insert([
+                'id_galeria' => $id_galeria,
+                'caminho' => '/imagens/' . $nome_da_imagem,
+                'data' => date('Y-m-d H:i:s')
+            ]);
+            move_uploaded_file($_FILES["imagem"]["tmp_name"], $caminho);
 
-           $db = \Config\Database::connect();
-           $db->table('fotos')->insert([
-               'id_galeria' => $id_galeria,
-               'caminho' => '/imagens/' . $nome_da_imagem,
-               'data' => date('Y-m-d H:i:s')
-           ]);
-           move_uploaded_file($_FILES["imagem"]["tmp_name"], $caminho);
-           return redirect()->to("/galeria/$id_galeria");
-       }
+            return redirect()->to("/galeria/$id_galeria");
+        }
     }
 
 
