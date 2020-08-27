468b9e70ab9fc94c52cec582bc7850406afc5eb9
pfraga <pfraga@epungo.com.br>
2020-08-27 12:57:13 -0300

    Refatora view template.

--- app/Views/template.php
+++ app/Views/template.php
@@ -3,6 +3,8 @@
 
     <head>
         <title><?php echo $titulo ?? "G"; ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="/dist/css/styles.css">
     </head>
 
@@ -11,4 +13,5 @@
         <?php $this->renderSection('content'); ?>
         <script type="text/javascript" src="/dist/js/scripts.js"></script>
     </body>
+
 </html>
