810592bd62c1a0cae50ec174b76fef1b72ab7968
pfraga <pfraga@epungo.com.br>
2020-08-27 12:56:29 -0300

    Refatora view sobre, usando bootstrap.

--- app/Views/sobre.php
+++ app/Views/sobre.php
@@ -3,48 +3,191 @@
 <?php $this->extend('template'); ?>
 
 <?php $this->section('content'); ?>
-    <div>
-        <table>
-            <tr>
-                <th>Ferramentas Usadas</th>
-                <th>O que são</th>
-            </tr>
-            <tr>
-                <td>CodeIgniter 4</td>
-                <td>O CodeIgniter é um framework de desenvolvimento de aplicações em PHP. </td>
-            </tr>
-            <tr>
-                <td>JavaScript</td>
-                <td>JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.</td>
-            </tr>
-                <td>PHP 7.3</td>
-                <td>PHP é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações presentes e atuantes no lado do servidor, capazes de gerar conteúdo dinâmico na World Wide Web.</td>
-            </tr>
-            </tr>
-                <td>HTML</td>
-                <td>HTML é uma linguagem de marcação utilizada na construção de páginas na Web</td>
-            </tr>
-            </tr>
-                <td>CSS</td>
-                <td>Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web.</td>
-            </tr>
-            </tr>
-                <td>Gulp</td>
-                <td>O gulp é um kit de ferramentas JavaScript de código aberto criado por Eric Schoffstall usado como um sistema de criação de streaming no desenvolvimento web front-end.</td>
-            </tr>
-            </tr>
-                <td>Composer</td>
-                <td>O Composer é um gerenciador de pacotes no nível do aplicativo para a linguagem de programação PHP que fornece um formato padrão para gerenciar dependências do software PHP e bibliotecas necessárias.</td>
-            </tr>
-            </tr>
-                <td>NPM</td>
-                <td>O npm é um gerenciador de pacotes para a linguagem de programação JavaScript. </td>
-            </tr>
-            </tr>
-                <td>MariaDB</td>
-                <td>Banco de dados</td>
-            </tr>
 
-        </table>
+    <div class="container">
+
+        <br>
+
+        <h1>
+            Sobre este projeto
+        </h1>
+
+        <h3 style="text-align: justify;">
+            Este projeto implementa um sistema para a criação e gerenciamento de galerias usando as tecnologias abaixo:
+        </h3>
+
+        <br>
+
+        <div class="row">
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/codeigniter.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            CodeIgniter 4
+                        </h5>
+                        <p class="card-text">
+                            O CodeIgniter é um framework de desenvolvimento de aplicações em PHP.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/php.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            PHP 7.3
+                        </h5>
+                        <p class="card-text">
+                            PHP é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações presentes e atuantes no lado do servidor, capazes de gerar conteúdo dinâmico na World Wide Web.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/javascript.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            JavaScript
+                        </h5>
+                        <p class="card-text">
+                            JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/html.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            HTML
+                        </h5>
+                        <p class="card-text">
+                            HTML é uma linguagem de marcação utilizada na construção de páginas na Web.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/css.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            CSS
+                        </h5>
+                        <p class="card-text">
+                            Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/gulp.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Gulp
+                        </h5>
+                        <p class="card-text">
+                            O gulp é um kit de ferramentas JavaScript de código aberto criado por Eric Schoffstall usado como um sistema de criação de streaming no desenvolvimento web front-end.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/composer.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Composer
+                        </h5>
+                        <p class="card-text">
+                            O Composer é um gerenciador de pacotes no nível do aplicativo para a linguagem de programação PHP que fornece um formato padrão para gerenciar dependências do software PHP e bibliotecas necessárias.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/npm.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            NPM
+                        </h5>
+                        <p class="card-text">
+                            O npm é um gerenciador de pacotes para a linguagem de programação JavaScript.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/mariadb.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            MariaDB
+                        </h5>
+                        <p class="card-text">
+                            MariaDB é um SGDB que surgiu como fork do MySQL, criado pelo próprio fundador do projeto após sua aquisição pela Oracle.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/bootstrap.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Bootstrap 4
+                        </h5>
+                        <p class="card-text">
+                            Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web usando HTML, CSS e JavaScript.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/github.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            GitHub
+                        </h5>
+                        <p class="card-text">
+                            GitHub é uma plataforma de hospedagem de código-fonte com controle de versão usando o Git.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card-sobre" >
+                    <img class="card-img-top" src="/imagens/logos/jquery.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Jquery
+                        </h5>
+                        <p class="card-text">
+                            jQuery é uma biblioteca de funções JavaScript que interage com o HTML, desenvolvida para simplificar os scripts interpretados no navegador do cliente.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
+
 <?php $this->endSection(); ?>
